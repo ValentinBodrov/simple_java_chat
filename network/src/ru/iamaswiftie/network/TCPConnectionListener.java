982c12d4e3c38df883487dc42eb8744ba8f0067a
Valbod-I-am-a-Swiftie <valbod@mail.ru>
2019-02-14 18:00:46 +0300

    The first version

---
+++ network/src/ru/iamaswiftie/network/TCPConnectionListener.java
@@ -0,0 +1,9 @@
+package ru.iamaswiftie.network;
+
+public interface TCPConnectionListener {
+
+    void onConnectionReady(TCPConnection tcpConnection);
+    void onReceiveString(TCPConnection tcpConnection, String value);
+    void onDisconnect(TCPConnection tcpConnection);
+    void onException(TCPConnection tcpConnection, Exception e);
+}
